95502306d1aa27105bb4e72e61958f5bac9d71e5
Samyak Kamble <126759819+samyak2403@users.noreply.github.com>
2023-07-28 21:08:07 +0530

    Initial commit

---
+++ app/src/main/java/com/arrowwould/saga/models/InterfaceJava.java
@@ -0,0 +1,19 @@
+package com.arrowwould.saga.models;
+
+import android.webkit.JavascriptInterface;
+
+import com.arrowwould.saga.activities.CallActivity;
+
+public class InterfaceJava {
+
+    CallActivity callActivity;
+
+    public InterfaceJava(CallActivity callActivity) {
+        this.callActivity = callActivity;
+    }
+
+    @JavascriptInterface
+    public void onPeerConnected(){
+        callActivity.onPeerConnected();
+    }
+}
